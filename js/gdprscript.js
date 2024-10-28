1366b43653fe8fb3a9089fa02f626e3e7c533426
John Brodnax <JBrodnax320@jbrodnax320.local>
2024-10-28 13:15:59 -0500

    added gdprscript and other missing js

---
+++ js/gdprscript.js
@@ -0,0 +1,39 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  //let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// Script created for GDPR Compliance. Source code located: weebly/kings-banner
+
+document.cookie = "gdpr-kb=true; expires=Thu, 01 Jan 2999 12:00:00 GMT; path=/";
+window.AragornAnalytics && window.AragornAnalytics.initialize();
+
+
+}
+/*
+     FILE ARCHIVED ON 15:13:41 Jul 09, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:01:44 Oct 28, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.259 (2)
+  exclusion.robots: 0.044 (2)
+  exclusion.robots.policy: 0.02 (2)
+  esindex: 0.024 (2)
+  cdx.remote: 14.875 (2)
+  LoadShardBlock: 4282.585 (6)
+  PetaboxLoader3.resolve: 6372.841 (7)
+  PetaboxLoader3.datanode: 151.911 (8)
+  load_resource: 2332.775 (2)
+*/
